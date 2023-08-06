b9ebe47f9a821ff20fb804b18b0618e48dcddc8b
yara alhassan <alhassanyara2@gmail.com>
2023-08-06 00:27:32 -0700

    first commit

---
+++ lib/test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  //Test(Key? key) : super(key: key);
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: ElevatedButton(
+              onPressed: () async {},
+              child: Text("Upload Image"),
+            ))
+          ],
+        ));
+  }
+}
---
+++ lib/testtwo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TestTwo extends StatefulWidget {
+  @override
+  _TestTwoState createState() => _TestTwoState();
+}
+
+class _TestTwoState extends State<TestTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Test Two'),
+        ),
+        body: Column(
+          children: [],
+        ));
+  }
+}
